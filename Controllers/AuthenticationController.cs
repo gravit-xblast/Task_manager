34375bcf1d21d86ba1f9e578551c1b1b80fc0d77
gravit-xblast <novablast.quazarx@gmail.com>
2026-06-24 14:58:16 +0200

    Add cookie-based authorization

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -55,6 +55,7 @@ namespace Task_Manager.Controllers
         // POST /token
         // Authentification → retourne le JWT
         // -------------------------
+
         [AllowAnonymous]
         [HttpPost("Login")]
         [ProducesResponseType(typeof(Token), StatusCodes.Status200OK)]
@@ -72,14 +73,50 @@ namespace Task_Manager.Controllers
 
             var accessToken = _tokenService.CreateAccessToken(user);
 
-            // access_token, token_type, expires_in
-            return Ok(new Token
-            {
-                AccessToken = accessToken,
-                TokenType = "Bearer",
-                ExpiresIn = _tokenService.AccessTokenExpiresInSeconds
-            });
+            Response.Cookies.Append(
+                "AccessToken",
+                accessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,      // JavaScript ne peut pas l'accéder
+                    Secure = true,        // Transmis uniquement en HTTPS
+                    SameSite = SameSiteMode.Strict,
+                    MaxAge = TimeSpan.FromMinutes(60)
+
+                }
+            );
+            return Ok(new { message = "Connecté" });
         }
+
+
+
+
+
+        //[AllowAnonymous]
+        //[HttpPost("Login")]
+        //[ProducesResponseType(typeof(Token), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //public async Task<IActionResult> Login([FromBody] LoginRequest data)
+        //{
+        //    var user = await _userService.Authenticate(data.Email, data.Password);
+
+        //    if (user is null)
+        //        return Unauthorized(new { detail = "Email ou mot de passe incorrect" });
+
+        //    if (!user.IsActive)
+        //        return BadRequest(new { detail = "Compte désactivé" });
+
+        //    var accessToken = _tokenService.CreateAccessToken(user);
+
+        //    // access_token, token_type, expires_in
+        //    return Ok(new Token
+        //    {
+        //        AccessToken = accessToken,
+        //        TokenType = "Bearer",
+        //        ExpiresIn = _tokenService.AccessTokenExpiresInSeconds
+        //    });
+        //}
     }
 }
 
